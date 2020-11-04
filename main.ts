63009ee74ddbe9418a4323728358b87b787a0716
Matias-Githubsaccount <69017144+Matias-Githubsaccount@users.noreply.github.com>
2020-11-03 20:34:55 -0500

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,15 @@
+input.onGesture(Gesture.Shake, function () {
+    basic.showLeds(`
+        . . # . .
+        . . # . .
+        . . # . .
+        . . # . .
+        . . # . .
+        `)
+})
 let counter = 0
 for (let index = 0; index < 4; index++) {
-    counter += 1
+    counter += 113
 }
 basic.forever(function () {
 	
